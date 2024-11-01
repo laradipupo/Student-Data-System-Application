6bf9170fc7a9d6b1c62c56095f3f9ec39baf2cb5
Omolara Oladipupo <omolaraoladipupo11@gmail.com>
2024-11-01 23:29:53 +0000

    injected dependency

--- src/main/java/com/cbfproject/student_data_system/student/StudentController.java
+++ src/main/java/com/cbfproject/student_data_system/student/StudentController.java
@@ -1,5 +1,6 @@
 package com.cbfproject.student_data_system.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class StudentController {
 
     private final StudentService studentService;
 
+    @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
