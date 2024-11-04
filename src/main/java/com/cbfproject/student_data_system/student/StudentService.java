12064db7dfb936edb0991b55d59943bc7ce50ca3
Omolara Oladipupo <omolaraoladipupo11@gmail.com>
2024-11-04 23:41:05 +0000

    used interface in my StudentService

--- src/main/java/com/cbfproject/student_data_system/student/StudentService.java
+++ src/main/java/com/cbfproject/student_data_system/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.cbfproject.student_data_system.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,17 +10,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Lara",
-                        "Oladipupo",
-                        "lara@gmail.com",
-                        LocalDate.of(2000, Month.OCTOBER, 4),
-                        "Derbyshire",
-                        "Microbiology"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
