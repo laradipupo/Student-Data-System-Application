90552d8f4c83aabaa8a9901cd707c722903abf9d
Omolara Oladipupo <omolaraoladipupo11@gmail.com>
2024-11-04 23:34:49 +0000

    created repository layer

---
+++ src/main/java/com/cbfproject/student_data_system/student/StudentRepository.java
@@ -0,0 +1,8 @@
+package com.cbfproject.student_data_system.student;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
