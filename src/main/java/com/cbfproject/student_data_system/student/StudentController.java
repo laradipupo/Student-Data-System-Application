56669bc2caf5bb33502051bc13042b7a2c2da078
Omolara Oladipupo <omolaraoladipupo11@gmail.com>
2024-11-05 01:31:51 +0000

    implemented adding a new student

--- src/main/java/com/cbfproject/student_data_system/student/StudentController.java
+++ src/main/java/com/cbfproject/student_data_system/student/StudentController.java
@@ -1,9 +1,7 @@
 package com.cbfproject.student_data_system.student;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.Month;
@@ -23,5 +21,11 @@ public class StudentController {
     @GetMapping
     public List<Student> getStudents() {
         return studentService.getStudents();
+    }
+
+    @PostMapping
+    public String addStudent(@RequestBody Student student) {
+        studentService.addNewStudent(student);
+        return "Student added successfully";
     }
 }
